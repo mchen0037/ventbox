b360ee263778b6e78e941207a55cfb3c401d41c0
Mighty Chen <mchen73@ucmerced.edu>
2019-03-01 20:07:30 -0800

    included additional examples for flask

--- ventbox-back/server.py
+++ ventbox-back/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 # I'm not the best at Flask (or React.. (or anything, but.))
@@ -23,6 +23,66 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# This is another endpoint! If you visit 0.0.0.0:4000/test, you will see this.
+@app.route("/test")
+def test():
+    return "This is the test page!"
+
+# Let's connect this with our front end. There will be two buttons:
+# yes or no, and we will give a reply accordingly
+ANSWER_YES = "Thai food is awesome!!"
+ANSWER_NO = "Please leave."
+@app.route("/thaifood/<string:ans>")
+def thaifood(ans):
+    # Basically, we want our 'yes' button to send the user to our /thaifood/yes
+    # page and we want our 'no' button to send them to /thaifood/no
+    if ans == "yes":
+        return ANSWER_YES
+    elif ans == "no":
+        return ANSWER_NO
+    else:
+        return "error: please visit /thaifood/yes or /thaifood/no"
+
+# Alternatively, we can pass arguments by parsing information through
+# /grades?student=info&classname=moreinfo&...
+# Set up some info..
+info = {
+    'mighty': {
+        'oop':65,
+        'db': 11,
+        'robotics': 80
+    },
+    'vivian': {
+        'oop': 100,
+        'db': 99,
+        'robotics': 103
+    }
+}
+
+@app.route("/grades")
+def grades():
+    # These lines parse the address for the parameters.
+    student = str(request.args.get('student'))
+    classname = str(request.args.get('classname'))
+
+    # you can use this to debug, it'll show up in the terminal window where
+    # you ran server.py when you visit the certain website.
+    print(student)
+    print(classname)
+
+    # If there was no argument for the student or class, or if they aren't
+    # valid, print out an error message.
+    if (student is None or classname is None or student not in info.keys() or
+        classname not in ['oop', 'db', 'robotics']):
+        out = ("""Error: Student or classname doesn't exist, try
+                /grades?student=vivian&classname=oop""")
+        return out
+
+    # Otherwise, print out their grade in the class.
+    out = (student + '\'s grade in ' + classname + ' is ' +
+            str(info[student][classname]))
+    return out
 
 # We can use this to run the app on a specific server/port.
 if __name__ == "__main__":
