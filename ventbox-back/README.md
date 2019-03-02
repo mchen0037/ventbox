c7608b92fe6e809f9c2404ebfd1c84299edb38a3
Mighty Chen <mchen73@ucmerced.edu>
2019-03-01 19:31:43 -0800

    description of flask in comments

--- ventbox-back/README.md
+++ ventbox-back/README.md
@@ -3,6 +3,7 @@
 ## Using Python 3.6.7, any Python 3 should work?
 
 1. Create a virtual environment.
+
   *tldr; we don't want to install every dependency on our core machine--some things might not be compatible, it doesn't make sense to install EVERYTHING on EVERY project we ever deploy, etc. Virtual environment gives us a 'clean' version of python with just the default packages.*
 ```
 virtualenv -p python3 env
@@ -11,6 +12,7 @@ source env/bin/activate
   *once you run the source env/bin/activate, you can type ```python --version``` to see that it will use python 3.6.7 (or whatever) instead of the default on your machine (probably 2.7). Make sure you either remember to run this ```source env/bin/activate``` on every terminal window you open (for Flask) or just add the same line to the end of your ~/.bashrc file (every line in the ~/.bashrc file will run when you open a Terminal window.)*
 
 2. Install the Dependencies.
+
   *These are the dependencies that this project will be using. It is the same output as ```pip freeze > requirements.txt```, and describes which version of every package that we need for our server to run.*
 ```
 pip install -r requirements.txt
@@ -24,5 +26,5 @@ pip freeze > requirements.txt
 
 3. Run the server!
 ```
-FLASK_APP=hello.py flask run
+python server.py
 ```
--- ventbox-back/server.py
+++ ventbox-back/server.py
@@ -1,6 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
+# I'm not the best at Flask (or React.. (or anything, but.))
+# I think there's a lot of different ways to use flask--we can render HTML
+# pages when we call a certain function, or we can just use it as a
+# server which outputs information for us and connects to the database.
+
+# We do this because of security reasons--it's not good to have information
+# about our database on the frontend server. Rather when we click a button
+# on the front end, it should visit our backend website (giving it some or no
+# information), and based on which "endpoint" we visited, we will return
+# other information about the given variables
+
+# For example, if this was 'Catcourses,' we might want to visit the
+# <server>/grades&mightychen endpoint, which will query the database for
+# mightychen and then return the grades for that user.
+
+# This is an endpoint! To keep it simple for now, we'll keep this hello world
+# in. Usually our endpoints will also display some text, but in a JSON format
+# so that it is easier to read on our front end side. i.e:
+# https://api.cryptonator.com/api/ticker/btc-usd
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# We can use this to run the app on a specific server/port.
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=4000)
