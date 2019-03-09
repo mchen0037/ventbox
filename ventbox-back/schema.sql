57f586b6743520eeb66d28117be13011a18d2e1d
Mighty Chen <mchen73@ucmerced.edu>
2019-03-08 20:19:50 -0800

    update schema to the db

--- ventbox-back/schema.sql
+++ ventbox-back/schema.sql
@@ -1,22 +1,9 @@
-DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS tags_posts;
-DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS vents;
 
-CREATE TABLE posts(
-	id int,
-	story text,
-	likes int,
-	tag VARCHAR(50),
-	timeStamp TIMESTAMP,
-	polarity decimal(2,0)
-);
-
-CREATE TABLE tags_posts (
-	tag_id int,
-	post_id int
-);
-
-CREATE TABLE tag (
-	tag_id int,
-	tag VARCHAR(50)
+CREATE TABLE vents (
+	id SERIAL,
+	vent TEXT,
+	likes INTEGER,
+	polarity REAL,
+	timestamp TIMESTAMP
 );
