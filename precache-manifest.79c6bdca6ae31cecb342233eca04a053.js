f5345197c0e4bd3b283a8a5a3ec102b8c6ee830b
Mighty Chen <mchen73@ucmerced.edu>
2019-03-09 21:46:26 -0800

    Updates

---
+++ precache-manifest.79c6bdca6ae31cecb342233eca04a053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2179f2dcc44a0a1810d3",
+    "url": "/ventbox/static/js/runtime~main.2179f2dc.js"
+  },
+  {
+    "revision": "8628b1cad253991881c6",
+    "url": "/ventbox/static/js/main.8628b1ca.chunk.js"
+  },
+  {
+    "revision": "0bd248f46f1b721aab31",
+    "url": "/ventbox/static/js/2.0bd248f4.chunk.js"
+  },
+  {
+    "revision": "8628b1cad253991881c6",
+    "url": "/ventbox/static/css/main.06ede0eb.chunk.css"
+  },
+  {
+    "revision": "d7c7f2fbee50d51bc9a772cfaa45c493",
+    "url": "/ventbox/index.html"
+  }
+];
